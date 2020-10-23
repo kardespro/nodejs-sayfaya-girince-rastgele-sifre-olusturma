6e1673b74097e9a5c5d7de52a37970465276415e
kardespro <61694826+kardespro@users.noreply.github.com>
2020-10-23 22:01:19 +0400

    Create kardespro.js

---
+++ kardespro.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const generator = require("generate-password");
+const krds = express();
+
+//Modul kullandim
+krds.get("/api/sifre-olustur", function(req,res){
+var password = generator.generate({
+    length: 10,//sifrenin uzunlugu
+    numbers: true //rakamlardan oluşumu
+   //not:true aktiv demekdir
+});
+let result = [];
+//sayfada random sifreyi gosterelim
+res.json(password);
+//olusturulmus sifreyi konsola gonderelim
+console.log("======Sifre Olusturuldu====");
+console.log(password);
+console.log("=======kardespro.cf==========");
+});
+
+app.listen(6060);
+
+/*
+____  __.                .___                                  
+|    |/ _|____ _______  __| _/____   ___________________  ____  
+|      < \__  \\_  __ \/ __ |/ __ \ /  ___/\____ \_  __ \/  _ \ 
+|    |  \ / __ \|  | \/ /_/ \  ___/ \___ \ |  |_> >  | \(  <_> )
+|____|__ (____  /__|  \____ |\___  >____  >|   __/|__|   \____/ 
+        \/    \/           \/    \/     \/ |__|                 
+
+*/
+
